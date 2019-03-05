a64a85706dd8adce0d7a7bfe1b739e8d6b1105a7
Alexander Menk <a.menk@imi.de>
2019-03-05 10:18:40 +0100

    Step 1 - Testnothing

---
+++ php/test/GameTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+
+class GameTest extends \PHPUnit\Framework\TestCase
+{
+
+    public function testNothing()
+    {
+        $this->assertTrue(true);
+
+    }
+
+}
