fb4f11cf4cc64dcee581b51f0b8672d981faa56d
Alexander Menk <a.menk@imi.de>
2019-03-05 10:36:23 +0100

    Make runable -> copy over from GameRunner

--- php/test/GameTest.php
+++ php/test/GameTest.php
@@ -6,20 +6,50 @@ class GameTest extends \PHPUnit\Framework\TestCase
 
     public function testCanRunTheGameDeterministically()
     {
-        ob_start();
-        $this->runGame();
-        $output = ob_get_contents();
-        ob_end_clean();
+        $outputA = $this->getGameOutput();
+        $outputB = $this->getGameOutput();
 
-        $this->assertNotEmpty($output);
+        $this->assertSame($outputA, $outputB);
+
+
     }
 
     /**
      * @return mixed
      */
-    protected function runGame()
+    protected function runGame(): void
     {
-        return include __DIR__ . '/../GameRunner.php';
+        include_once __DIR__ . '/../Game.php';
+
+        $aGame = new Game();
+        $aGame->add("Chet");
+        $aGame->add("Pat");
+        $aGame->add("Sue");
+
+        do {
+            $aGame->roll(rand(0, 5) + 1);
+
+            if (rand(0, 9) == 7) {
+                $notAWinner = $aGame->wrongAnswer();
+            } else {
+                $notAWinner = $aGame->wasCorrectlyAnswered();
+            }
+        } while ($notAWinner);
+    }
+
+    /**
+     * @return false|string
+     */
+    protected function getGameOutput()
+    {
+        srand(1);
+
+        ob_start();
+        $this->runGame();
+        $output = ob_get_contents();
+        ob_end_clean();
+
+        return $output;
     }
 
 }
